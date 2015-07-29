d0e09b326c52cd51061cf24f4bd551b73de351ec
Stefano Pacifici <stefano@cliqz.com>
2015-07-29 14:27:32 +0200

    CLIQZ Build system in place using grunt task in gradle

---
+++ navigation/src/main/java/com/cliqz/navigation/Dummy.java
@@ -0,0 +1,9 @@
+package com.cliqz.navigation;
+
+/**
+ * Created by Stefano Pacifici on 29/07/15.
+ *
+ * Only need to ensure the library will be included in the final APK
+ */
+public class Dummy {
+}
