bbfd56cc3ac6622b55af484e1740acb5caf76d20
Kevin Read <kevin@cliqz.com>
2015-07-30 17:15:52 +0200

    Fix path to new location of freshtab

--- app/src/main/java/acr/browser/lightning/constant/Constants.java
+++ app/src/main/java/acr/browser/lightning/constant/Constants.java
@@ -23,7 +23,7 @@ public final class Constants {
 	public static final String STARTPAGE_SEARCH = "https://startpage.com/do/search?language=english&query=";
 	public static final String ASK_SEARCH = "http://www.ask.com/web?qsrc=0&o=0&l=dir&qo=lightningBrowser&q=";
 	// public static final String HOMEPAGE = "about:home";
-	public static final String HOMEPAGE = "file:///android_asset/freshtab/freshtab.html";
+	public static final String HOMEPAGE = "file:///android_asset/page/freshtab.html";
 	public static final String BAIDU_SEARCH = "https://www.baidu.com/s?wd=";
 	public static final String YANDEX_SEARCH = "https://yandex.ru/yandsearch?lr=21411&text=";
 	public static final String EXTERNAL_STORAGE = Environment.getExternalStorageDirectory()
