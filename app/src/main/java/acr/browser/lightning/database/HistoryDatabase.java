58b7a44f0346ea3018ffa1028c157ff06fcabd6b
Stefano Pacifici <stefano@cliqz.com>
2015-10-26 18:17:20 +0100

    HistoryItem(s) with timestamp

--- app/src/main/java/acr/browser/lightning/database/HistoryDatabase.java
+++ app/src/main/java/acr/browser/lightning/database/HistoryDatabase.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,240 +16,244 @@ import acr.browser.lightning.R;
 
 public class HistoryDatabase extends SQLiteOpenHelper {
 
-	// All Static variables
-	// Database Version
-	private static final int DATABASE_VERSION = 3;
+    // All Static variables
+    // Database Version
+    private static final int DATABASE_VERSION = 3;
 
-	// Database Name
-	public static final String DATABASE_NAME = "historyManager";
+    // Database Name
+    public static final String DATABASE_NAME = "historyManager";
 
-	// HistoryItems table name
+    // HistoryItems table name
     private static final String TABLE_HISTORY = "history";
 
-	// HistoryItems Table Columns names
-	public static final String KEY_ID = "id";
-	public static final String KEY_URL = "url";
-	public static final String KEY_TITLE = "title";
-	public static final String KEY_TIME_VISITED = "time";
-	public static final String KEY_COUNT_VISITED = "visits";
+    // HistoryItems Table Columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_URL = "url";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_TIME_VISITED = "time";
+    public static final String KEY_COUNT_VISITED = "visits";
 
     private static SQLiteDatabase mDatabase;
 
-	private static HistoryDatabase mInstance;
+    private static HistoryDatabase mInstance;
 
-	public static HistoryDatabase getInstance(Context context) {
-		if (mInstance == null || mInstance.isClosed()) {
-			mInstance = new HistoryDatabase(context);
-		}
-		return mInstance;
-	}
+    public static HistoryDatabase getInstance(Context context) {
+        if (mInstance == null || mInstance.isClosed()) {
+            mInstance = new HistoryDatabase(context);
+        }
+        return mInstance;
+    }
 
-	private HistoryDatabase(Context context) {
-		super(context.getApplicationContext(), DATABASE_NAME, null, DATABASE_VERSION);
-		mDatabase = this.getWritableDatabase();
-	}
+    private HistoryDatabase(Context context) {
+        super(context.getApplicationContext(), DATABASE_NAME, null, DATABASE_VERSION);
+        mDatabase = this.getWritableDatabase();
+    }
 
-	// Creating Tables
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		String CREATE_HISTORY_TABLE = "CREATE TABLE " + TABLE_HISTORY + "(" + KEY_ID
-				+ " INTEGER PRIMARY KEY," + KEY_URL + " TEXT," + KEY_TITLE + " TEXT,"
-				+ KEY_TIME_VISITED + " INTEGER" + ", " + KEY_COUNT_VISITED + " INTEGER)";
-		db.execSQL(CREATE_HISTORY_TABLE);
-		final String CREATE_HISTORY_INDEX_URL = "CREATE INDEX IF NOT EXISTS urlIndex ON " +
-				TABLE_HISTORY + "(" + KEY_URL + " COLLATE NOCASE)";
-		db.execSQL(CREATE_HISTORY_INDEX_URL);
-		final String CREATE_VISITS_INDEX = "CREATE INDEX IF NOT EXISTS countIndex ON " +
-				TABLE_HISTORY + "(" + KEY_COUNT_VISITED + ")";
-		db.execSQL(CREATE_VISITS_INDEX);
+    // Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_HISTORY_TABLE = "CREATE TABLE " + TABLE_HISTORY + "(" + KEY_ID
+                + " INTEGER PRIMARY KEY," + KEY_URL + " TEXT," + KEY_TITLE + " TEXT,"
+                + KEY_TIME_VISITED + " INTEGER" + ", " + KEY_COUNT_VISITED + " INTEGER)";
+        db.execSQL(CREATE_HISTORY_TABLE);
+        final String CREATE_HISTORY_INDEX_URL = "CREATE INDEX IF NOT EXISTS urlIndex ON " +
+                TABLE_HISTORY + "(" + KEY_URL + " COLLATE NOCASE)";
+        db.execSQL(CREATE_HISTORY_INDEX_URL);
+        final String CREATE_VISITS_INDEX = "CREATE INDEX IF NOT EXISTS countIndex ON " +
+                TABLE_HISTORY + "(" + KEY_COUNT_VISITED + ")";
+        db.execSQL(CREATE_VISITS_INDEX);
 
 
-	}
+    }
 
-	// Upgrading database
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		if (oldVersion == 2) {
-			db.execSQL("ALTER TABLE " + TABLE_HISTORY + " ADD " + KEY_COUNT_VISITED + " INTEGER");
-			final String CREATE_VISITS_INDEX = "CREATE INDEX IF NOT EXISTS countIndex ON " +
-					TABLE_HISTORY + "(" + KEY_COUNT_VISITED + ")";
-			db.execSQL(CREATE_VISITS_INDEX);
-		} else {
-		// Drop older table if it exists
-		db.execSQL("DROP TABLE IF EXISTS " + TABLE_HISTORY);
-		// Create tables again
-		onCreate(db);
-	}
-	}
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion == 2) {
+            db.execSQL("ALTER TABLE " + TABLE_HISTORY + " ADD " + KEY_COUNT_VISITED + " INTEGER");
+            final String CREATE_VISITS_INDEX = "CREATE INDEX IF NOT EXISTS countIndex ON " +
+                    TABLE_HISTORY + "(" + KEY_COUNT_VISITED + ")";
+            db.execSQL(CREATE_VISITS_INDEX);
+        } else {
+            // Drop older table if it exists
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_HISTORY);
+            // Create tables again
+            onCreate(db);
+        }
+    }
 
-	public boolean isClosed() {
-		return mDatabase == null || !mDatabase.isOpen();
-	}
+    public boolean isClosed() {
+        return mDatabase == null || !mDatabase.isOpen();
+    }
 
-	@Override
-	public synchronized void close() {
-		if (mDatabase != null) {
-			mDatabase.close();
-		}
-		super.close();
-	}
+    @Override
+    public synchronized void close() {
+        if (mDatabase != null) {
+            mDatabase.close();
+        }
+        super.close();
+    }
 
-	public synchronized void deleteHistoryItem(String url) {
-		mDatabase.delete(TABLE_HISTORY, KEY_URL + " = ?", new String[] { url });
-	}
+    public synchronized void deleteHistoryItem(String url) {
+        mDatabase.delete(TABLE_HISTORY, KEY_URL + " = ?", new String[]{url});
+    }
 
-	public synchronized void visitHistoryItem(String url, String title) {
+    public synchronized void visitHistoryItem(String url, String title) {
 
-		Cursor q = mDatabase.query(false, TABLE_HISTORY, new String[] { KEY_URL, KEY_COUNT_VISITED },
-				KEY_URL + " = ?", new String[] { url }, null, null, null, "1");
-		if (q.getCount() > 0) {
-			q.moveToFirst();
-			final ContentValues values = new ContentValues();
-		values.put(KEY_TITLE, title);
-		values.put(KEY_TIME_VISITED, System.currentTimeMillis());
-			values.put(KEY_COUNT_VISITED, q.getInt(1) + 1);
-			mDatabase.update(TABLE_HISTORY, values, KEY_URL + " = ?", new String[] { url });
+        Cursor q = mDatabase.query(false, TABLE_HISTORY, new String[]{KEY_URL, KEY_COUNT_VISITED},
+                KEY_URL + " = ?", new String[]{url}, null, null, null, "1");
+        if (q.getCount() > 0) {
+            q.moveToFirst();
+            final ContentValues values = new ContentValues();
+            values.put(KEY_TITLE, title);
+            values.put(KEY_TIME_VISITED, System.currentTimeMillis());
+            values.put(KEY_COUNT_VISITED, q.getInt(1) + 1);
+            mDatabase.update(TABLE_HISTORY, values, KEY_URL + " = ?", new String[]{url});
 
-		} else {
-			addHistoryItem(new HistoryItem(url, title));
-		}
-		q.close();
-	}
+        } else {
+            addHistoryItem(new HistoryItem(url, title));
+        }
+        q.close();
+    }
 
-	public synchronized List<HistoryItem> getTopSites(int limit) {
-		if (limit < 1) {
-			limit = 1;
-		} else if (limit > 100) {
-			limit = 100;
-		}
-		List<HistoryItem> itemList = new ArrayList<>();
-		String selectQuery = "SELECT * FROM " + TABLE_HISTORY + " ORDER BY " + KEY_COUNT_VISITED
-				+ " DESC LIMIT " + limit;
+    public synchronized List<HistoryItem> getTopSites(int limit) {
+        if (limit < 1) {
+            limit = 1;
+        } else if (limit > 100) {
+            limit = 100;
+        }
+        List<HistoryItem> itemList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_HISTORY + " ORDER BY " + KEY_COUNT_VISITED
+                + " DESC LIMIT " + limit;
 
-		Cursor cursor = mDatabase.rawQuery(selectQuery, null);
-		int counter = 0;
-		if (cursor.moveToFirst()) {
-			do {
-				HistoryItem item = new HistoryItem();
-				item.setID(Integer.parseInt(cursor.getString(0)));
-				item.setUrl(cursor.getString(1));
-				item.setTitle(cursor.getString(2));
-				item.setImageId(R.drawable.ic_history);
-				itemList.add(item);
-				counter++;
-			} while (cursor.moveToNext() && counter < 100);
-		}
-		cursor.close();
-		return itemList;
+        Cursor cursor = mDatabase.rawQuery(selectQuery, null);
+        int counter = 0;
+        if (cursor.moveToFirst()) {
+            do {
+                HistoryItem item = new HistoryItem();
+                item.setID(Integer.parseInt(cursor.getString(0)));
+                item.setUrl(cursor.getString(1));
+                item.setTitle(cursor.getString(2));
+                item.setTimestamp(cursor.getLong(3));
+                item.setImageId(R.drawable.ic_history);
+                itemList.add(item);
+                counter++;
+            } while (cursor.moveToNext() && counter < 100);
+        }
+        cursor.close();
+        return itemList;
 
-	}
+    }
 
     private synchronized void addHistoryItem(HistoryItem item) {
-		ContentValues values = new ContentValues();
-		values.put(KEY_URL, item.getUrl());
-		values.put(KEY_TITLE, item.getTitle());
-		values.put(KEY_TIME_VISITED, System.currentTimeMillis());
-		mDatabase.insert(TABLE_HISTORY, null, values);
-	}
+        ContentValues values = new ContentValues();
+        values.put(KEY_URL, item.getUrl());
+        values.put(KEY_TITLE, item.getTitle());
+        values.put(KEY_TIME_VISITED, System.currentTimeMillis());
+        mDatabase.insert(TABLE_HISTORY, null, values);
+    }
 
-	String getHistoryItem(String url) {
-		Cursor cursor = mDatabase.query(TABLE_HISTORY, new String[] { KEY_ID, KEY_URL, KEY_TITLE },
-				KEY_URL + " = ?", new String[] { url }, null, null, null, null);
-		String m = null;
-		if (cursor != null) {
-			cursor.moveToFirst();
-			m = cursor.getString(0);
+    String getHistoryItem(String url) {
+        Cursor cursor = mDatabase.query(TABLE_HISTORY, new String[]{KEY_ID, KEY_URL, KEY_TITLE},
+                KEY_URL + " = ?", new String[]{url}, null, null, null, null);
+        String m = null;
+        if (cursor != null) {
+            cursor.moveToFirst();
+            m = cursor.getString(0);
 
-			cursor.close();
-		}
-		return m;
-	}
+            cursor.close();
+        }
+        return m;
+    }
 
-	public List<HistoryItem> findItemsContaining(final String search, int limit) {
-		if (limit <= 0) {
-			limit = 5;
-		}
-		List<HistoryItem> itemList = new ArrayList<>();
-		String selectQuery = "SELECT * FROM " + TABLE_HISTORY + " WHERE " + KEY_TITLE + " LIKE '%"
-				+ search + "%' OR " + KEY_URL + " LIKE '%" + search + "%' " + "ORDER BY "
-				+ KEY_TIME_VISITED + " DESC LIMIT " + limit;
-		Cursor cursor = mDatabase.rawQuery(selectQuery, null);
+    public List<HistoryItem> findItemsContaining(final String search, int limit) {
+        if (limit <= 0) {
+            limit = 5;
+        }
+        List<HistoryItem> itemList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_HISTORY + " WHERE " + KEY_TITLE + " LIKE '%"
+                + search + "%' OR " + KEY_URL + " LIKE '%" + search + "%' " + "ORDER BY "
+                + KEY_TIME_VISITED + " DESC LIMIT " + limit;
+        Cursor cursor = mDatabase.rawQuery(selectQuery, null);
 
-		int n = 0;
-		if (cursor.moveToFirst()) {
-			do {
-				HistoryItem item = new HistoryItem();
-				item.setID(Integer.parseInt(cursor.getString(0)));
-				item.setUrl(cursor.getString(1));
-				item.setTitle(cursor.getString(2));
-				item.setImageId(R.drawable.ic_history);
-				itemList.add(item);
-				n++;
-			} while (cursor.moveToNext() && n < 5);
-		}
-		cursor.close();
-		return itemList;
-	}
+        int n = 0;
+        if (cursor.moveToFirst()) {
+            do {
+                HistoryItem item = new HistoryItem();
+                item.setID(Integer.parseInt(cursor.getString(0)));
+                item.setUrl(cursor.getString(1));
+                item.setTitle(cursor.getString(2));
+                item.setTimestamp(cursor.getLong(3));
+                item.setImageId(R.drawable.ic_history);
+                itemList.add(item);
+                n++;
+            } while (cursor.moveToNext() && n < 5);
+        }
+        cursor.close();
+        return itemList;
+    }
 
-	public List<HistoryItem> getLastHundredItems() {
-		List<HistoryItem> itemList = new ArrayList<>();
-		String selectQuery = "SELECT * FROM " + TABLE_HISTORY + " ORDER BY " + KEY_TIME_VISITED
-				+ " DESC";
+    public List<HistoryItem> getLastHundredItems() {
+        List<HistoryItem> itemList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_HISTORY + " ORDER BY " + KEY_TIME_VISITED
+                + " DESC";
 
-		Cursor cursor = mDatabase.rawQuery(selectQuery, null);
-		int counter = 0;
-		if (cursor.moveToFirst()) {
-			do {
-				HistoryItem item = new HistoryItem();
-				item.setID(Integer.parseInt(cursor.getString(0)));
-				item.setUrl(cursor.getString(1));
-				item.setTitle(cursor.getString(2));
-				item.setImageId(R.drawable.ic_history);
-				itemList.add(item);
-				counter++;
-			} while (cursor.moveToNext() && counter < 100);
-		}
-		cursor.close();
-		return itemList;
-	}
+        Cursor cursor = mDatabase.rawQuery(selectQuery, null);
+        int counter = 0;
+        if (cursor.moveToFirst()) {
+            do {
+                HistoryItem item = new HistoryItem();
+                item.setID(Integer.parseInt(cursor.getString(0)));
+                item.setUrl(cursor.getString(1));
+                item.setTitle(cursor.getString(2));
+                item.setTimestamp(cursor.getLong(3));
+                item.setImageId(R.drawable.ic_history);
+                itemList.add(item);
+                counter++;
+            } while (cursor.moveToNext() && counter < 100);
+        }
+        cursor.close();
+        return itemList;
+    }
 
-	public List<HistoryItem> getAllHistoryItems() {
-		List<HistoryItem> itemList = new ArrayList<>();
-		String selectQuery = "SELECT  * FROM " + TABLE_HISTORY + " ORDER BY " + KEY_TIME_VISITED
-				+ " DESC";
+    public List<HistoryItem> getAllHistoryItems() {
+        List<HistoryItem> itemList = new ArrayList<>();
+        String selectQuery = "SELECT  * FROM " + TABLE_HISTORY + " ORDER BY " + KEY_TIME_VISITED
+                + " DESC";
 
-		Cursor cursor = mDatabase.rawQuery(selectQuery, null);
+        Cursor cursor = mDatabase.rawQuery(selectQuery, null);
 
-		if (cursor.moveToFirst()) {
-			do {
-				HistoryItem item = new HistoryItem();
-				item.setID(Integer.parseInt(cursor.getString(0)));
-				item.setUrl(cursor.getString(1));
-				item.setTitle(cursor.getString(2));
-				item.setImageId(R.drawable.ic_history);
-				itemList.add(item);
-			} while (cursor.moveToNext());
-		}
-		cursor.close();
-		return itemList;
-	}
+        if (cursor.moveToFirst()) {
+            do {
+                HistoryItem item = new HistoryItem();
+                item.setID(Integer.parseInt(cursor.getString(0)));
+                item.setUrl(cursor.getString(1));
+                item.setTitle(cursor.getString(2));
+                item.setTimestamp(cursor.getLong(3));
+                item.setImageId(R.drawable.ic_history);
+                itemList.add(item);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return itemList;
+    }
 
-	public synchronized int updateHistoryItem(HistoryItem item) {
+    public synchronized int updateHistoryItem(HistoryItem item) {
 
-		ContentValues values = new ContentValues();
-		values.put(KEY_URL, item.getUrl());
-		values.put(KEY_TITLE, item.getTitle());
-		values.put(KEY_TIME_VISITED, System.currentTimeMillis());
-		return mDatabase.update(TABLE_HISTORY, values, KEY_ID + " = ?",
-				new String[] { String.valueOf(item.getId()) });
-	}
+        ContentValues values = new ContentValues();
+        values.put(KEY_URL, item.getUrl());
+        values.put(KEY_TITLE, item.getTitle());
+        values.put(KEY_TIME_VISITED, System.currentTimeMillis());
+        return mDatabase.update(TABLE_HISTORY, values, KEY_ID + " = ?",
+                new String[]{String.valueOf(item.getId())});
+    }
 
-	public int getHistoryItemsCount() {
-		String countQuery = "SELECT * FROM " + TABLE_HISTORY;
-		Cursor cursor = mDatabase.rawQuery(countQuery, null);
-		int n = cursor.getCount();
-		cursor.close();
+    public int getHistoryItemsCount() {
+        String countQuery = "SELECT * FROM " + TABLE_HISTORY;
+        Cursor cursor = mDatabase.rawQuery(countQuery, null);
+        int n = cursor.getCount();
+        cursor.close();
 
-		return n;
-	}
+        return n;
+    }
 }
--- app/src/main/java/acr/browser/lightning/database/HistoryItem.java
+++ app/src/main/java/acr/browser/lightning/database/HistoryItem.java
@@ -8,20 +8,21 @@ import android.support.annotation.NonNull;
 
 public class HistoryItem implements Comparable<HistoryItem> {
 
-	// private variables
-	private int mId = 0;
-	private String mUrl = "";
-	private String mTitle = "";
-	private String mFolder = "";
-	private Bitmap mBitmap = null;
-	private int mImageId = 0;
-	private int mOrder = 0;
+    // private variables
+    private int mId = 0;
+    private String mUrl = "";
+    private String mTitle = "";
+    private String mFolder = "";
+    private Bitmap mBitmap = null;
+    private int mImageId = 0;
+    private int mOrder = 0;
+    private long mTimestamp;
     private boolean mIsFolder = false;
 
-	// Empty constructor
-	public HistoryItem() {
+    // Empty constructor
+    public HistoryItem() {
 
-	}
+    }
 
     public HistoryItem(HistoryItem item) {
         this.mUrl = item.mUrl;
@@ -29,164 +30,179 @@ public class HistoryItem implements Comparable<HistoryItem> {
         this.mFolder = item.mFolder;
         this.mOrder = item.mOrder;
         this.mIsFolder = item.mIsFolder;
+        this.mTimestamp = item.mTimestamp;
     }
 
-	// constructor
-	public HistoryItem(int id, String url, String title) {
-		this.mId = id;
-		this.mUrl = url;
-		this.mTitle = title;
-		this.mBitmap = null;
-	}
+    // constructor
+    public HistoryItem(int id, String url, String title) {
+        this.mId = id;
+        this.mUrl = url;
+        this.mTitle = title;
+        this.mBitmap = null;
+        this.mTimestamp = System.currentTimeMillis();
+    }
 
-	// constructor
-	public HistoryItem(String url, String title) {
-		this.mUrl = url;
-		this.mTitle = title;
-		this.mBitmap = null;
-	}
+    // constructor
+    public HistoryItem(String url, String title) {
+        this.mUrl = url;
+        this.mTitle = title;
+        this.mBitmap = null;
+        this.mTimestamp = System.currentTimeMillis();
+    }
 
-	// constructor
-	public HistoryItem(String url, String title, int imageId) {
-		this.mUrl = url;
-		this.mTitle = title;
-		this.mBitmap = null;
-		this.mImageId = imageId;
-	}
+    // constructor
+    public HistoryItem(String url, String title, int imageId) {
+        this.mUrl = url;
+        this.mTitle = title;
+        this.mBitmap = null;
+        this.mImageId = imageId;
+        this.mTimestamp = System.currentTimeMillis();
+    }
 
-	public StringBuilder toJsonString(final StringBuilder sb) {
-		if (mUrl != null /*&& !url.isEmpty() && (
+    public StringBuilder toJsonString(final StringBuilder sb) {
+        if (mUrl != null /*&& !url.isEmpty() && (
                         (title.toLowerCase(locale).contains(q)) ||
                                 (url.toLowerCase(locale).contains(q))
                         )*/) {
-			final String title;
-			if (mTitle.contains("\"")) {
-				title = mTitle.replace("\"", "\\\"");
-			} else {
-				title = mTitle;
-			}
-			sb.append("{\"title\":\"")
-					.append(title)
-					.append("\",\"url\":\"")
-					.append(mUrl)
-					.append("\", \"score\": 0}");
-		}
-		return sb;
-	}
+            final String title;
+            if (mTitle.contains("\"")) {
+                title = mTitle.replace("\"", "\\\"");
+            } else {
+                title = mTitle;
+            }
+            sb.append("{\"title\":\"")
+                    .append(title)
+                    .append("\",\"url\":\"")
+                    .append(mUrl)
+                    .append("\",\"timestamp\":")
+                    .append(mTimestamp)
+                    .append(", \"score\": 0}");
+        }
+        return sb;
+    }
 
-	// getting ID
-	public int getId() {
-		return this.mId;
-	}
+    // getting ID
+    public int getId() {
+        return this.mId;
+    }
 
-	public int getImageId() {
-		return this.mImageId;
-	}
+    public int getImageId() {
+        return this.mImageId;
+    }
 
-	// setting id
-	public void setID(int id) {
-		this.mId = id;
-	}
+    // setting id
+    public void setID(int id) {
+        this.mId = id;
+    }
 
-	public void setImageId(int id) {
-		this.mImageId = id;
-	}
+    public void setImageId(int id) {
+        this.mImageId = id;
+    }
 
-	public void setBitmap(Bitmap image) {
-		mBitmap = image;
-	}
+    public void setBitmap(Bitmap image) {
+        mBitmap = image;
+    }
 
-	public void setFolder(String folder) {
-		mFolder = (folder == null) ? "" : folder;
-	}
+    public void setFolder(String folder) {
+        mFolder = (folder == null) ? "" : folder;
+    }
 
-	public void setOrder(int order) {
-		mOrder = order;
-	}
+    public void setOrder(int order) {
+        mOrder = order;
+    }
 
-	public int getOrder() {
-		return mOrder;
-	}
+    public int getOrder() {
+        return mOrder;
+    }
 
-	public String getFolder() {
-		return mFolder;
-	}
+    public String getFolder() {
+        return mFolder;
+    }
 
-	public Bitmap getBitmap() {
-		return mBitmap;
-	}
+    public Bitmap getBitmap() {
+        return mBitmap;
+    }
 
-	// getting name
-	public String getUrl() {
-		return this.mUrl;
-	}
+    // getting name
+    public String getUrl() {
+        return this.mUrl;
+    }
 
-	// setting name
-	public void setUrl(String url) {
-		this.mUrl = (url == null) ? "" : url;
-	}
+    // setting name
+    public void setUrl(String url) {
+        this.mUrl = (url == null) ? "" : url;
+    }
 
-	// getting phone number
-	public String getTitle() {
-		return this.mTitle;
-	}
+    // getting phone number
+    public String getTitle() {
+        return this.mTitle;
+    }
 
-	// setting phone number
-	public void setTitle(String title) {
-		this.mTitle = (title == null) ? "" : title;
-	}
+    // getting the timestamp
+    public long getTimestamp() {
+        return mTimestamp;
+    }
+
+    // setting phone number
+    public void setTitle(String title) {
+        this.mTitle = (title == null) ? "" : title;
+    }
 
     public void setIsFolder(boolean isFolder) {
         mIsFolder = isFolder;
+    }
+
+    public void setTimestamp(long timestamp) {
+        mTimestamp = timestamp;
     }
 
     public boolean isFolder() {
         return mIsFolder;
     }
 
-	@Override
-	public String toString() {
-		return mTitle;
-	}
+    @Override
+    public String toString() {
+        return mTitle;
+    }
 
-	@Override
-	public int compareTo(@NonNull HistoryItem another) {
-		return mTitle.compareTo(another.mTitle);
-	}
+    @Override
+    public int compareTo(@NonNull HistoryItem another) {
+        return mTitle.compareTo(another.mTitle);
+    }
 
-	@Override
-	public boolean equals(Object o) {
+    @Override
+    public boolean equals(Object o) {
 
-		if (this == o) {
-			return true;
-		}
-		if (o == null || ((Object) this).getClass() != o.getClass()) {
-			return false;
-		}
+        if (this == o) {
+            return true;
+        }
+        if (o == null || ((Object) this).getClass() != o.getClass()) {
+            return false;
+        }
 
-		HistoryItem that = (HistoryItem) o;
+        HistoryItem that = (HistoryItem) o;
 
-		if (mId != that.mId) {
-			return false;
-		}
-		if (mImageId != that.mImageId) {
-			return false;
-		}
-		if (mBitmap != null ? !mBitmap.equals(that.mBitmap) : that.mBitmap != null) {
-			return false;
-		}
-		return mTitle.equals(that.mTitle) && mUrl.equals(that.mUrl);
-	}
+        if (mId != that.mId) {
+            return false;
+        }
+        if (mImageId != that.mImageId) {
+            return false;
+        }
+        if (mBitmap != null ? !mBitmap.equals(that.mBitmap) : that.mBitmap != null) {
+            return false;
+        }
+        return mTitle.equals(that.mTitle) && mUrl.equals(that.mUrl);
+    }
 
-	@Override
-	public int hashCode() {
+    @Override
+    public int hashCode() {
 
-		int result = mId;
-		result = 31 * result + mUrl.hashCode();
-		result = 31 * result + mTitle.hashCode();
-		result = 31 * result + (mBitmap != null ? mBitmap.hashCode() : 0);
-		result = 31 * result + mImageId;
+        int result = mId;
+        result = 31 * result + mUrl.hashCode();
+        result = 31 * result + mTitle.hashCode();
+        result = 31 * result + (mBitmap != null ? mBitmap.hashCode() : 0);
+        result = 31 * result + mImageId;
 
-		return result;
-	}
+        return result;
+    }
 }
