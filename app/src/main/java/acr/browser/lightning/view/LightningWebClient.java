34cb570ec6cf2e211a256f8a111faaa867ee242a
Stefano Pacifici <stefano@cliqz.com>
2016-04-05 17:21:16 +0200

    Doesn't show "trampoline" as title

--- app/src/main/java/acr/browser/lightning/view/LightningWebClient.java
+++ app/src/main/java/acr/browser/lightning/view/LightningWebClient.java
@@ -153,8 +153,11 @@ class LightningWebClient extends WebViewClient {
             mEventBus.post(new BrowserEvents.UpdateUrl(url,true));
             view.postInvalidate();
         }
-        if (view.getTitle() != null && !view.getTitle().isEmpty()
-                && !view.getTitle().contains(TrampolineConstants.CLIQZ_TRAMPOLINE_GOTO)) {
+        final String title = view.getTitle();
+        if (title != null &&
+                !title.isEmpty() &&
+                !title.startsWith(TrampolineConstants.CLIQZ_TRAMPOLINE_GOTO) &&
+                !TrampolineConstants.CLIQZ_TRAMPOLINE_GOTO.equals(url)) {
             mLightningView.mTitle.setTitle(view.getTitle());
             mEventBus.post(new Messages.UpdateTitle());
         }
