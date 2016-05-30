80e92815a5fad128243be6be0c7baf48262574a6
Stefano Pacifici <stefano@cliqz.com>
2016-05-30 11:30:10 +0200

    AB-472 Automation to check multiple cards are visible in landscape mode

--- spec/tests/should_start_v17+.js
+++ spec/tests/should_start_v17+.js
@@ -1,14 +1,16 @@
 'use strict';
 'api level: 17,18,19';
 
+const testpage = "https://cdn.cliqz.com/mobile/browser/tests/testpage.html";
+
 require("../helpers/setup");
 
 var wd = require("wd"),
     _ = require('underscore'),
     Promise = require('promise'),
-    serverConfig = require('../helpers/servers');
+    serverConfig = require('../helpers/servers'),
+    assert = require('chai').assert;
 
-console.log(serverConfig['cliqz_api_level']);
 let id = (id) => process.env.APPIUM_API_LEVEL < 18 ? id : `com.cliqz.browser:id/${id}`;
 
 describe("Browser", function () {
@@ -39,5 +41,44 @@ describe("Browser", function () {
         .click()
       .elementByAccessibilityId("Search Bar")
         .should.eventually.exist;
+  });
+
+  it("should navigate to test page", function() {
+    return driver
+      .waitForElementByAccessibilityId("Search Bar")
+        .sendKeys(testpage)
+        .pressDeviceKey(66)
+      .context("WEBVIEW_com.cliqz.browser")
+      .elementById("datetime")
+        .should.eventually.exist
+      .context("NATIVE_APP")
+  });
+
+  it("should show multiple cards when in landscape", function() {
+    const storage = { width: 0, x: 0 };
+    return driver
+      .waitForElementByAccessibilityId("Search Bar")
+        .sendKeys("pippo")
+      .setOrientation('LANDSCAPE')
+      .context("WEBVIEW_com.cliqz.browser")
+      .findWindowWithTitle("developer tool")
+      .waitForElementById('cliqz-results')
+        .then(function(el) {
+          return driver.getSize(el);
+        })
+        .then(function(size) {
+          storage.width = parseInt(size.width);
+          return driver;
+        })
+      .eval("document.getElementsByClassName('frame')[2].style.left")
+        .then(function (value) {
+          storage.x = parseInt(value);
+          return driver;
+        })
+      .context("NATIVE_APP")
+        .then(function() {
+          assert.isBelow(storage.x, storage.width, "The third card is not visible");
+          return driver;
+        });
   });
 });
