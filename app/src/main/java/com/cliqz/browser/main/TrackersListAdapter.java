47bc78f6cfb031a15239b8478f3a4017a2c95986
Ravjit Singh Uppal <ravjit@cliqz.com>
2016-08-10 14:57:55 +0200

    Design touch up

--- app/src/main/java/com/cliqz/browser/main/TrackersListAdapter.java
+++ app/src/main/java/com/cliqz/browser/main/TrackersListAdapter.java
@@ -1,9 +1,13 @@
 package com.cliqz.browser.main;
 
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cliqz.browser.R;
@@ -16,9 +20,14 @@ import java.util.ArrayList;
 public class TrackersListAdapter extends RecyclerView.Adapter<TrackersListAdapter.ViewHolder> {
 
     private final ArrayList<TrackerDetailsModel> trackerDetails;
+    private final boolean isIncognito;
+    private final Context context;
 
-    public TrackersListAdapter(ArrayList<TrackerDetailsModel> trackerDetails) {
+    public TrackersListAdapter(ArrayList<TrackerDetailsModel> trackerDetails, boolean isIncognito,
+                               Context context) {
         this.trackerDetails = trackerDetails;
+        this.isIncognito = isIncognito;
+        this.context = context;
     }
 
     @Override
@@ -30,8 +39,12 @@ public class TrackersListAdapter extends RecyclerView.Adapter<TrackersListAdapte
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        final int textColor = isIncognito ? R.color.normal_tab_primary_color : R.color.incognito_tab_primary_color;
         holder.trackerName.setText(trackerDetails.get(position).companyName);
         holder.trackerCount.setText(Integer.toString(trackerDetails.get(position).trackerCount));
+        holder.trackerName.setTextColor(ContextCompat.getColor(context, textColor));
+        holder.trackerCount.setTextColor(ContextCompat.getColor(context, textColor));
+        holder.infoImage.getDrawable().setColorFilter(ContextCompat.getColor(context, textColor), PorterDuff.Mode.SRC_ATOP);
     }
 
     @Override
@@ -43,11 +56,13 @@ public class TrackersListAdapter extends RecyclerView.Adapter<TrackersListAdapte
 
         public final TextView trackerName;
         public final TextView trackerCount;
+        public final ImageView infoImage;
 
         public ViewHolder(View view) {
             super(view);
             trackerName = (TextView) view.findViewById(R.id.tracker_name);
             trackerCount = (TextView) view.findViewById(R.id.tracker_count);
+            infoImage = (ImageView) view.findViewById(R.id.info);
         }
     }
 }
