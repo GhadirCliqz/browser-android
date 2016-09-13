43bb7767b7cbcbe732662ffe37c49894f8388e62
ravjit-cliqz <ravjit@cliqz.com>
2016-09-13 15:16:39 +0200

    AB-530 integrated FB sdk (#233)

--- app/src/main/java/com/cliqz/browser/app/BrowserApp.java
+++ app/src/main/java/com/cliqz/browser/app/BrowserApp.java
@@ -12,6 +12,8 @@ import com.cliqz.browser.di.components.AppComponent;
 import com.cliqz.browser.di.components.DaggerAppComponent;
 import com.cliqz.browser.di.modules.AppModule;
 import com.cliqz.browser.utils.LookbackWrapper;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.squareup.leakcanary.LeakCanary;
 
 public class BrowserApp extends Application {
@@ -23,6 +25,9 @@ public class BrowserApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // Initialize the SDK before executing any other operations,
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         LeakCanary.install(this);
         LookbackWrapper.init(this);
         buildDepencyGraph();
